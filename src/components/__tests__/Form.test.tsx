6abad6f9fdb7faaaca2ca4fd0f2eefe04ee6172e
OleksiiMyzgin <oleksii.myzgin@flexiana.com>
2020-12-16 11:45:05 +0100

    test: add more tests

--- src/components/__tests__/Form.test.tsx
+++ src/components/__tests__/Form.test.tsx
@@ -23,28 +23,89 @@ describe('Form tests', () => {
     expect(getByTestId('form')).toBeInTheDocument();
   });
 
-  it('check process', () => {
-    global.alert = jest.fn();
-    const { getByTestId, getByRole } = render(
-      <FormProvider>
-        <Form />
-      </FormProvider>,
-    );
+  describe('check process', () => {
+    it('should alert number', () => {
+      global.alert = jest.fn();
+      const { getByTestId, getByRole } = render(
+        <FormProvider>
+          <Form />
+        </FormProvider>,
+      );
 
-    const firstNumber = getByTestId('firstNumber').querySelector('input') as HTMLInputElement;
-    const secondNumber = getByTestId('secondNumber').querySelector('input') as HTMLInputElement;
-    const submitBtn = getByRole('button', { name: /Process/i});
+      const firstNumber = getByTestId('firstNumber').querySelector('input') as HTMLInputElement;
+      const secondNumber = getByTestId('secondNumber').querySelector('input') as HTMLInputElement;
+      const submitBtn = getByRole('button', { name: /Process/i});
 
-    expect(firstNumber).toBeInTheDocument();
-    expect(secondNumber).toBeInTheDocument();
-    expect(submitBtn).toBeInTheDocument();
+      fireEvent.change(firstNumber, { target: { value: '1' } });
+      fireEvent.change(secondNumber, { target: { value: '6' } });
 
-    fireEvent.change(firstNumber, { target: { value: '1' } });
-    fireEvent.change(secondNumber, { target: { value: '6' } });
+      fireEvent.click(submitBtn);
 
-    fireEvent.click(submitBtn);
+      expect(global.alert).toHaveBeenCalledTimes(1);
+      expect(global.alert).toHaveBeenCalledWith(2);
+    });
 
-    expect(global.alert).toHaveBeenCalledTimes(1);
-    expect(global.alert).toHaveBeenCalledWith(2);
+    it('should alert test "Both values must be numbers"', () => {
+      global.alert = jest.fn();
+      const { getByTestId, getByRole } = render(
+        <FormProvider>
+          <Form />
+        </FormProvider>,
+      );
+
+      const firstNumber = getByTestId('firstNumber').querySelector('input') as HTMLInputElement;
+      const secondNumber = getByTestId('secondNumber').querySelector('input') as HTMLInputElement;
+      const submitBtn = getByRole('button', { name: /Process/i});
+
+      fireEvent.change(firstNumber, { target: { value: 'text' } });
+      fireEvent.change(secondNumber, { target: { value: '6' } });
+
+      fireEvent.click(submitBtn);
+
+      expect(global.alert).toHaveBeenCalledTimes(1);
+      expect(global.alert).toHaveBeenCalledWith("Both values must be numbers");
+    });
+
+    it('should alert test "Input can not be empty"', () => {
+      global.alert = jest.fn();
+      const { getByTestId, getByRole } = render(
+        <FormProvider>
+          <Form />
+        </FormProvider>,
+      );
+
+      const firstNumber = getByTestId('firstNumber').querySelector('input') as HTMLInputElement;
+      const secondNumber = getByTestId('secondNumber').querySelector('input') as HTMLInputElement;
+      const submitBtn = getByRole('button', { name: /Process/i});
+
+      fireEvent.change(firstNumber, { target: { value: '' } });
+      fireEvent.change(secondNumber, { target: { value: '' } });
+
+      fireEvent.click(submitBtn);
+
+      expect(global.alert).toHaveBeenCalledTimes(1);
+      expect(global.alert).toHaveBeenCalledWith("Input can not be empty");
+    });
+    it('should alert test "The number should be bigger than 0"', () => {
+      global.alert = jest.fn();
+      const { getByTestId, getByRole } = render(
+        <FormProvider>
+          <Form />
+        </FormProvider>,
+      );
+
+      const firstNumber = getByTestId('firstNumber').querySelector('input') as HTMLInputElement;
+      const secondNumber = getByTestId('secondNumber').querySelector('input') as HTMLInputElement;
+      const submitBtn = getByRole('button', { name: /Process/i});
+
+      fireEvent.change(firstNumber, { target: { value: '0' } });
+      fireEvent.change(secondNumber, { target: { value: '5' } });
+
+      fireEvent.click(submitBtn);
+
+      expect(global.alert).toHaveBeenCalledTimes(1);
+      expect(global.alert).toHaveBeenCalledWith("The number should be bigger than 0");
+    });
   });
+
 });
